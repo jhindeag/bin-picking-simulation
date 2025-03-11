beb9cbde5274c3579f172efb3045cc24cdd98a61
Duc Anh Le <jhindeag@gmail.com>
2025-03-11 12:15:28 +0100

    linking nlohmann_json

--- coppeliasim-cpp-client/coppeliasim-cpp-client.cpp
+++ coppeliasim-cpp-client/coppeliasim-cpp-client.cpp
@@ -1,5 +1,6 @@
 ﻿#include "coppeliasim-cpp-client.h"
-
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
 
 int main()
 {
@@ -11,21 +12,23 @@ int main()
 
 		client.startSimulation();
 		client.log_msg("Simulation started.");
-		const int obj_handle = client.getObjectHandle("Cuboid");
+		// const int obj_handle = client.getObjectHandle("Cuboid");
+		const int scanner_handle = client.getObjectHandle("fast3DLaserScanner");
 
-		while(client.isConnected())
+		while (client.isConnected())
 		{
-			const coppeliasim_cpp::Pose obj_pose = client.getObjectPose(obj_handle);
-			std::cout << "Object position: " << obj_pose.position.x << ", " << obj_pose.position.y << ", " << obj_pose.position.z << std::endl;
-			std::cout << "Object orientation: " << obj_pose.orientation.alpha << ", " << obj_pose.orientation.beta << ", " << obj_pose.orientation.gamma << std::endl;
+			// const coppeliasim_cpp::Pose obj_pose = client.getObjectPose(obj_handle);
+			// std::cout << "Object position: " << obj_pose.position.x << ", " << obj_pose.position.y << ", " << obj_pose.position.z << std::endl;
+			// std::cout << "Object orientation: " << obj_pose.orientation.alpha << ", " << obj_pose.orientation.beta << ", " << obj_pose.orientation.gamma << std::endl;
 
-			const coppeliasim_cpp::Position obj_pos = client.getObjectPosition(obj_handle);
+			const coppeliasim_cpp::Position obj_pos = client.getObjectPosition(scanner_handle);
 			std::cout << "Object position: " << obj_pos.x << ", " << obj_pos.y << ", " << obj_pos.z << std::endl;
-
-			const coppeliasim_cpp::Orientation obj_ori = client.getObjectOrientation(obj_handle);
-			std::cout << "Object orientation: " << obj_ori.alpha << ", " << obj_ori.beta << ", " << obj_ori.gamma << std::endl;
-			
-			Sleep(100);
+			const std::string cborSignal = client.getStringSignal("measuredData");
+			json j_from_cbor = json::from_cbor(cborSignal);
+			std::cout << "Measured data: " << j_from_cbor.dump(4) << std::endl;
+			// const coppeliasim_cpp::Orientation obj_ori = client.getObjectOrientation(obj_handle);
+			// std::cout << "Object orientation: " << obj_ori.alpha << ", " << obj_ori.beta << ", " << obj_ori.gamma << std::endl;
+			Sleep(5000);
 		}
 	}
 	else
